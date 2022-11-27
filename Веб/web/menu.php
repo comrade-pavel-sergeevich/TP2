6dc146a2cd7cf2099bf78dadc6c95e7b4058e8f1
Pavel <holms001@mail.ru>
2022-11-27 20:45:02 +0300

    test menu

--- Веб/web/menu.php
+++ Веб/web/menu.php
@@ -1,3 +1,110 @@
-<?php
-include("Menu.html");
-?>
+
+<div class="menu" style="padding:0;margin:0;height:80px;">
+<div class="downskoe-menu" style="margin-left:20px; width: 97.5%;"><ul class="food menu" style="justify-content: flex-start;"><li class="menu-item">Сырники</li><li class="menu-item"></li></ul></div>
+</div>
+
+<div style="height:80%;width: 97%;border-radius:6px;display:flex;flex-direction:row;justify-content: space-between;margin-left: 20px;margin-right: 25px;margin-top: 70px;padding-bottom:40px">
+<div style="width: 20%;border-radius:6px; background-color:#222121;display:flex;flex-direction:column;"><p style="color: #ebb572;margin-left: -100px;">Страна Производитель:</p>
+    <div style="display:flex;flex-direction:column">
+        <input type="radio" id="contactChoice1"
+               name="contact" style="margin-left: -180px">
+        <label for="contactChoice1" style="color: #ebb572; margin-left: 70px;margin-top: -16px; margin-bottom: 20px;">Россия</label>
+        <input type="radio" id="contactChoice2"
+               name="contact" style="margin-left: -180px">
+        <label for="contactChoice2"style="color: #ebb572; margin-left: 70px;margin-top: -16px;margin-bottom: 20px">Сша</label>
+        <input type="radio" id="contactChoice3"
+               name="contact" style="margin-left: -180px">
+        <label for="contactChoice3"style="color: #ebb572; margin-left: 70px;margin-top: -16px;margin-bottom: 20px">Казахстан</label>
+    </div>
+    <p style="color: #ebb572;margin-left: -230px;">Цена:</p>
+    <div class="slidecontainer">
+        <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+      </div>
+    <div style="display:flex;flex-direction:row; margin-top: 100px;">
+        <button class = "button_menu" style="margin-left:20px ;">Применить</button>
+        <button class = "button_menu" style="margin-left: 55px">Очистить</button>
+
+    </div>
+</div>
+<div style="height:100%;width: 75%;border-radius:6px; display:flex;flex-direction:column;">
+<div style="height: 100%;width: 100%;border-radius:6px;display:flex;flex-direction: row;flex-wrap: wrap;height:fit-content">
+    <?php
+    for($i=1; $i<=100; $i++){echo(
+        "<div class=\"menu_izdeliya\"><img src=\"https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg\"width=301px height=201px style=\"border-radius: 10px;\"><button style=\"border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;\"><img src=\"https://cdn-icons-png.flaticon.com/512/1374/1374128.png\" width=\"40\" ></button>        <div style=\"border: 2px solid black;border-radius: 5px;border-style: dotted\">            <p class = \"menu_text\"> Сырники с клюквенным вареньем и сметаной</p>   <div style=\"display: flex; flex-direction: row;justify-content: space-between;\">            <p class = \"price\"> 300 руб. </p>            <p class = \"price\"> 300 гр </p>         </div>        </div></div>"
+    );};
+    ?>    
+
+    <!-- <div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div><div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div><div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div><div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div><div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div><div class="menu_izdeliya">
+        <img src="https://prostokvashino.ru/upload/iblock/d28/d28e1b22ab38bfcce66f54a1a80e7526.jpg"
+        width=301px height=201px style="border-radius: 10px;">
+        <button style="border-radius: 7px;margin-left: 240px;margin-top: -50px;cursor: pointer;background: transparent;"><img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" width="40" ></button>
+        <div style="border: 2px solid black;border-radius: 5px;border-style: dotted">
+            <p class = "menu_text"> Сырники с клюквенным вареньем и сметаной</p>  
+            <div style="display: flex; flex-direction: row;justify-content: space-between;">
+            <p class = "price"> 300 руб. </p>
+            <p class = "price"> 300 гр </p>
+            </div>
+        </div>
+    </div> -->
+    
+    
+</div>
+<div style="height: 15%;width: 100%;border-radius:6px; background-color:#222121;display:flex;flex-direction: column;"><p  text-align="left" style="color: #ebb572;"> Информация об изделии</p></div>
+</div>
+<footer></footer>	
+
